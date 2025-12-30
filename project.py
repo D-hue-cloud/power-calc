3750194d9b6c0b9bb66da43d571f8e62716b2230
D-hue-cloud <dinugaratnayake@gmail.com>
2025-12-30 17:28:46 +0000

    Add files via upload

---
+++ project.py
@@ -0,0 +1,4 @@
+base=int(input("Enter the base (a.k.a the number you would like to exponentiate)"))
+index=int(input("Enter what your power (The number you exponentiate by) would like to be:  "))
+ans=base**index
+print("The answer is: ", ans)
